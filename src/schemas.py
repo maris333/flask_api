9a1a09a5e82fed3bd89c0b87200b481c7cd9d784
maris333 <118438276+maris333@users.noreply.github.com>
2023-10-26 10:38:42 +0200

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,11 +1,3 @@
-"""
-class TrainingSchema(ma.Schema):
-    _id = fields.fields.Integer()
-    name = fields.fields.Str()
-    date = fields.fields.DateTime(format='%Y-%m-%d')
-    duration = fields.fields.Integer()
-    note = fields.fields.Str()
-"""
 from src import ma
 from flask_marshmallow import fields
 
